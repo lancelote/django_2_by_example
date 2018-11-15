2ad6a95b3ca98fb9f8e2fdd5272585aafe0a0a1b
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-11-15 12:10:16 +0300

    Fix webdriver download link for Linux

--- scripts/setup_webdriver.sh
+++ scripts/setup_webdriver.sh
@@ -1,4 +1,4 @@
-wget "https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-$([[ $OSTYPE =~ darwin ]] && echo macos || echo linux).tar.gz"
+wget "https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-$([[ $OSTYPE =~ darwin ]] && echo macos || echo linux64).tar.gz"
 mkdir -p vendor
 tar -xzf geckodriver*.tar.gz -C vendor/
 rm geckodriver*.tar.gz
