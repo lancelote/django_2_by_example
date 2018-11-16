7326ae8b215663a11c38b0eb679b6cc5b3af1b3a
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-11-16 10:32:36 +0300

    Extract os and version to variables

--- scripts/setup_webdriver.sh
+++ scripts/setup_webdriver.sh
@@ -1,4 +1,7 @@
-wget "https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-$([[ $OSTYPE =~ darwin ]] && echo macos || echo linux64).tar.gz"
+version="v0.23.0"
+os="$([[ $OSTYPE =~ darwin ]] && echo macos || echo linux64)"
+
+wget "https://github.com/mozilla/geckodriver/releases/download/${version}/geckodriver-${version}-${os}.tar.gz"
 mkdir -p vendor
 tar -xzf geckodriver*.tar.gz -C vendor/
 rm geckodriver*.tar.gz
